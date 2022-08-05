51db519d81ca58de6bed3ef9e1ea0155fe2ec0df
franciscofjs <prof.juniofernandes@gmail.com>
2022-08-05 14:04:34 -0300

    Primeito commit. Iniciando o projeto

---
+++ src/main/java/br/edu/ifsertaope/cs/projetoweb/Projetopw2Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifsertaope.cs.projetoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetopw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetopw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsertaope/cs/projetoweb/Projetopw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsertaope.cs.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetopw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
